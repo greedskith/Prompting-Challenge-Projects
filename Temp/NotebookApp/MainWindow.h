d0e043f0c9fe6632b6068106c411d424af8a6983
Skith <174881372+greedskith@users.noreply.github.com>
2025-12-19 16:37:15 +0530

    Challenge 1 Code

--- Temp/NotebookApp/MainWindow.h
+++ Temp/NotebookApp/MainWindow.h
@@ -2,7 +2,10 @@
 #define MAINWINDOW_H
 
 #include "Note.h"
+#include <QLabel>
+#include <QLineEdit>
 #include <QListWidget>
+#include <QListWidgetItem>
 #include <QMainWindow>
 #include <QTextEdit>
 #include <vector>
@@ -18,16 +21,21 @@ private slots:
   void onNewNote();
   void onSaveNote();
   void onDeleteNote();
-  void onNoteSelected(QListWidgetItem *item);
+  void onNoteSelected(QListWidgetItem *current, QListWidgetItem *previous);
+  void onSearchTextChanged(const QString &text);
 
 private:
   void setupUI();
+  void applyStyles();
   void refreshNoteList();
 
+  QLabel *m_headerLabel;
+  QLineEdit *m_searchBar;
   QListWidget *m_noteList;
   QTextEdit *m_noteEditor;
   std::vector<Note> m_notes;
   int m_currentNoteIndex; // -1 if no note selected
+  QString m_searchFilter;
 };
 
 #endif // MAINWINDOW_H
